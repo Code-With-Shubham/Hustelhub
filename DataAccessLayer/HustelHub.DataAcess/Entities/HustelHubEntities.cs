0147b426b469c6727a6aa01ac9699d5c75a17aa0
Shubham <shubhammanohare88@gmail.com>
2025-04-03 15:02:49 +0530

    Database Connection Added.

---
+++ DataAccessLayer/HustelHub.DataAcess/Entities/HustelHubEntities.cs
@@ -0,0 +1,27 @@
+﻿using HustelHub.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HustelHub.DataAccess.Entities
+{
+   
+
+    public class HustelHubEntities : ApplicationDbContext
+    {
+        public HustelHubEntities()
+        {
+
+        }
+        public HustelHubEntities(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+    }
+
+}
